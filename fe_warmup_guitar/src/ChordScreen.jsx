aa4adb7ea561a2515de1c8097f566dd4c08bbd5d
Nasser <nassermassadimi@gmail.com>
2022-05-08 15:21:06 +0200

    Enable the user to update the value of the timer The user can changes the value of the timer using the form provided For now, he needs to fill in the input with a value in milliseconds In the next iterations, we will use  - and + buttons to increase or decrease the timer
    We created a class Timer to create / update new Timer easily
    To avoid confusion the used formerly known as timer has been renamed
    [delay, set_delay]
    
    We added a new property to this state : new_timer. It's a boolean
    and start with a value of false. We need this boolean to makes want a
    new timer.
     - This property switch to "true" when the user complete the timer form
     - When this property is true :
        *we stop the current_timer (the one about to be triggered)
        *we create a new Timer with the updated value for the state delay
        *Thanks to side effects, the property start() of this new Timer is
        triggered after its inception.

--- fe_warmup_guitar/src/ChordScreen.jsx
+++ fe_warmup_guitar/src/ChordScreen.jsx
@@ -5,15 +5,19 @@ const ChordScreen = () => {
     
     const [note, set_note] = React.useState("A");
     const [chord, set_chord] = React.useState(6);
-    const [timer, set_timer] = React.useState({
-        time_typed : "",
-        time_confirmed : 10000
+    const [delay, set_delay] = React.useState({
+        time_typed : "",            // Updated everytime while the user type a new value for the delay
+        time_confirmed : 5000,      // Upadted when the user confirm the new delay
+        new_timer : false,          // switch to true when we update the timer. switch back to false when the current_timer has been updated with the new value of delay.time_confirmed
     });
     
-    let zeWorld;
+    const [current_timer, set_current_timer] = React.useState(null);
+    // required to store the current Timer ID.
+    // We will use it to identify the timer we need to stop with the stop() method of the class Timer
+
+
 
     // Change the chords in a random way.
-
     const mysterious_note = () => {
         console.log("I have been called");
         const all_notes = ["A","Ab", "A#", "B", "Bb", "C", "C#", "D", "Db", "D#", "E", "Eb","F", "F#", "G", "Gb", "G#"];
@@ -24,46 +28,89 @@ const ChordScreen = () => {
         // Choose a random chord
         const random_chord = all_chords[Math.round(Math.random() * all_chords.length)];
         set_chord(random_chord);
-        zeWorld = setTimeout(mysterious_note, timer.time_confirmed);   
-        console.log(timer.time_confirmed);
-        console.log(zeWorld);
         console.log(random_note + random_chord);
-        return zeWorld;   
+        // Create a new Timer (will be started in the useEffect (see below))
+        const next_timer = new Timer(delay.time_confirmed);
+        set_current_timer(next_timer);
     };
 
-    // ISSUE : Why this useEffect is called twice ?
-    React.useEffect( ( ) => {
-        console.log("useMe!");
-        mysterious_note();
-    }, [])
+    // THE TIMER CONSTRUCTOR - 
+    // Everytime x second we create a new object with this constructor
+    class Timer {
+        constructor(delay) {
+            this.delay = delay;
+            this.timerId = null;   
+        }
+        // a function to trigger this timer. When delay is elapsed we tri
+        start () {
+            this.timerId = setTimeout(mysterious_note, this.delay);  // With timerId we have a way to identify the setTimeout function, we need that to stop the right timer with the method stop()
+            console.log(`TimerID(start) : ${this.timerId}`);
+            return this.timerId;
+        }
+        // a function to stop this delay.
+        stop () {
+            clearTimeout(this.timerId);
+            console.log(`We are cancelling this timer : ${this.timerId}`);
+        };
+    }
 
-    // Update the value of timer 
+
+
+// SIDE EFFECTS ()
+    // Create the initial timer during the first render
+    React.useEffect ( () => {
+        let initial_timer = new Timer(delay.time_confirmed);
+        set_current_timer(initial_timer);
+    }, []);
+
+    // Trigger the current_timer we just set using the method start().
+    React.useEffect( () => {
+        if (current_timer !== null) {
+            console.log(`current Timer : ${JSON.stringify(current_timer)}`);
+            current_timer.start();
+        }
+    }
+    , [current_timer]);
+    
+    // When the user set a new value for the timer, we stop the current timer and set a new one with the updated value for delay.
+    React.useEffect( () => {
+        console.log(`new delay : ${JSON.stringify(delay)}`);
+        if (delay.new_timer) {          // with this conditional we are sure to update the timer if and only if the user explicitly send the form.
+            // 1 - We stop the former timer
+            current_timer.stop();
+            // 2 - We create a new timer with the new value of timer
+            let new_timer = new Timer(delay.time_confirmed);
+            set_current_timer(new_timer);
+            // 3 - We switch the property new_timer to false
+            set_delay({
+                ...delay,
+                new_timer : false,
+            })
+        }
+    }, [delay.time_confirmed]);
+
+
+    
+// HANDLER FUNCTIONS
+    // Update the value of delay 
     const update_timer = (event) => {
-        set_timer({
-            ...timer,
+        set_delay({
+            ...delay,
             time_typed : event.target.value
         });
     };
 
-    // Confirm the value of the timer
-    const confirm_timer_value = (event) => {
-        clearTimeout(zeWorld);
-        zeWorld = null;
+    // Confirm the value of the delay
+    const confirm_timer_value = (event) => {        
         event.preventDefault();
-        set_timer({
+        set_delay({
             time_typed : 0,           
-            time_confirmed : timer.time_typed 
-        })
-/*         set_timer({
-            ...timer,             
-            time_typed : 0, // When the form is sent we resent the time typed.
-        }) */
-        ;
+            time_confirmed : delay.time_typed,
+            new_timer : true, 
+        });
     }
 
-    React.useEffect( () => {
-        console.log(timer);
-    }, [timer]);
+
     return (
         <>
             {!note || !chord ? (
@@ -74,7 +121,7 @@ const ChordScreen = () => {
                     <span>{chord}</span>
                     <h5>Adjust the delay</h5>
                     <form onSubmit={confirm_timer_value}>
-                        <input type="text" value={timer.time_typed} onChange= {update_timer} />
+                        <input type="text" value={delay.time_typed} onChange= {update_timer} />
                         <input type="submit" value="Set a new timer" />
                     </form>
                 </>
