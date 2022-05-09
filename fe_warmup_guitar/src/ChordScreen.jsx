05a69fc36bf7f931ea98260307d7ea2ccfc5358e
Nasser <nassermassadimi@gmail.com>
2022-05-09 20:43:55 +0200

    Implement and display the right solution The right_solution() is working well. Now we just need to set the timers properly. There are 3 timer: TIMER1 - The timer selected by the user TIMER2 - The timer before a new note is displayed TIMER3 - The timer to display the correct answer.
    To make it works, we will need to add buffer time between each stage
    EXAMPLE
    TIMER1 : the user define a timer of 10s.
    TIMER2 : the note is displayed during 15s
    TIMER 3: The correct answer is displayed after 10s untill the next
    note is displayed. In this example, the correct answer is then visible
    for 5s.

--- fe_warmup_guitar/src/ChordScreen.jsx
+++ fe_warmup_guitar/src/ChordScreen.jsx
@@ -17,32 +17,33 @@ const ChordScreen = () => {
 
 
 // STATES
-    const [note, set_note] = React.useState("A");
-    const [chord, set_chord] = React.useState(4);
+    const [note, set_note] = React.useState("E");
+    const [chord, set_chord] = React.useState(1);
     const [delay, set_delay] = React.useState({
         time_typed : "",            // Updated everytime while the user type a new value for the delay
         time_confirmed : 5000,      // Upadted when the user confirm the new delay
         new_timer : false,          // switch to true when we update the timer. switch back to false when the current_timer has been updated with the new value of delay.time_confirmed
     });
-    
-    const [current_timer, set_current_timer] = React.useState(null);
+
     // required to store the current Timer ID.
     // We will use it to identify the timer we need to stop with the stop() method of the class Timer
+    const [current_timer, set_current_timer] = React.useState(null);
+    const [answer, set_answer] = React.useState(); //to display the fret to play to get the note expected (number_of_intervals). 
+
 
 
 //CORE FUNCTIONS
-    // Change the chords in a random way.
+
+    // DISPLAY RANDOM NOTE TO PLAY ON A RANDOM CHORD
     const mysterious_note = () => {
         console.log("I have been called");
         // Choose a random note
         const random_index_note = Math.floor(Math.random() * all_notes.length);
-/*         console.log(`RANDOM NOTE CHORD : ${random_index_note}`) */
         const random_note = all_notes[random_index_note];
         set_note(random_note);
         // Choose a random chord
         const chords_name = Object.keys(all_chords);
         const random_index_chord = Math.floor(Math.random() * chords_name.length);
-/*         console.log(`RANDOM INDEX CHORD : ${random_index_chord}`) */
         const random_chord_number = chords_name[random_index_chord];
         set_chord(random_chord_number);
         console.log(random_note + random_chord_number);
@@ -51,18 +52,15 @@ const ChordScreen = () => {
         set_current_timer(next_timer);
     };
 
-// Display the solution => The fret where the user should put his/her finger to produce the right note.
+
+// DISPLAY THE RIGHT FRET TO BE PLAYED => The fret where the user should put his/her finger to produce the right note.
     // input : 1 - the number of the chord (1, 2, 3...etc). | 2 - The note to play (A, B, C#...etc),
     // output :  the fret to play on the given chord (a number.)
-    // What we need to get to this output ?
-        // The starting point / open_chord_note : the name of the note on fret 0 (E,A,D,G,B or E), 
-        // we just need to calculate the "distance" between the open_chord_note and the note_to_play to find the fret_to_play.
-        // EXAMPLE: E5 ---> 
-
-    const right_fret = (chord_number, note_to_play) => {
+    const right_fret = (number_of_chord, note_to_play) => {
         let fret_to_play;
+        console.log(`INPUT RIGHT_FRET: ${number_of_chord} ${note_to_play}`);
         //a - note_to_play = E   || open_chord_note = 5 ==> We deduce that open_chord_note = A.
-        const open_chord_note = all_chords[chord_number];  // output expected : an open chord letter (E, A, D, G, B, E)
+        const open_chord_note = all_chords[number_of_chord];  // output expected : an open chord letter (E, A, D, G, B, E)
         // b - Between A and E there is ALWAYS a distance of 7 half-step/ frets (this the meat of the algorithm).
         const chords_grid = {
             A : 0,
@@ -83,15 +81,26 @@ const ChordScreen = () => {
             G_sharp : 11,
             Ab : 11,
         }
-        // c - the value to return (fret_to_play) is 7!
-        const number_of_intervals = 12;                 // Why 12 ? Because 12 is the total number of interval (remember C_sharp = Db are the same note!)
+        // c - the computation of the fret to play
+        console.log (`CHORD TO PLAY : ${chords_grid[note_to_play]}`);
+        console.log (`CHORD TO PLAY : ${chords_grid[open_chord_note]}`);
+
+        const number_of_intervals = 12;                 // Why 12 ? Because 12 is the total number of interval (remember C_sharp = Db are the same note!). If we count 12 intervals, we get back to the same note. 
         if (chords_grid[note_to_play] > chords_grid[open_chord_note]) {
+            // We just need to compute the interval between the 2 chords to know which fret should be played
             fret_to_play = chords_grid[note_to_play] - chords_grid[open_chord_note];
         }else {
-            fret_to_play = 12 + chords_grid[open_chord_note] -chords_grid[note_to_play]; 
+            // In that case we need to reach (it makes sense if you write the object chord_grid on a piece of paper and count the interval - try with open_chord_note = G and note_to_play = C for example
+            fret_to_play = number_of_intervals - chords_grid[open_chord_note] + chords_grid[note_to_play]; 
         }
+        console.log(`FRET TO PLAY : ${fret_to_play}`);
         return fret_to_play;
     };
+
+
+
+
+
 
     // THE TIMER CONSTRUCTOR - 
     // Everytime x second we create a new object with this constructor
@@ -100,14 +109,14 @@ const ChordScreen = () => {
             this.delay = delay;
             this.timerId = null;   
         }
-        // a function to trigger this timer. When delay is elapsed we tri
-        start () {
+        // a function to trigger this timer. When delay is elapsed we call mysterious_note().
+        start() {
             this.timerId = setTimeout(mysterious_note, this.delay);  // With timerId we have a way to identify the setTimeout function, we need that to stop the right timer with the method stop()
             console.log(`TimerID(start) : ${this.timerId}`);
             return this.timerId;
         }
         // a function to stop this delay.
-        stop () {
+        stop() {
             clearTimeout(this.timerId);
             console.log(`We are cancelling this timer : ${this.timerId}`);
         };
@@ -148,6 +157,10 @@ const ChordScreen = () => {
         }
     }, [delay.time_confirmed]);
 
+    React.useEffect ( () => {
+        set_answer(right_fret(chord, note));
+    }, [note, chord])
+
 
     
 // HANDLER FUNCTIONS
@@ -178,6 +191,7 @@ const ChordScreen = () => {
                 <>
                     <span>{note}</span>
                     <span>{chord}</span>
+                    <p>The answer {answer}</p>
                     <h5>Adjust the delay</h5>
                     <form onSubmit={confirm_timer_value}>
                         <input type="text" value={delay.time_typed} onChange= {update_timer} />
@@ -190,3 +204,8 @@ const ChordScreen = () => {
 };
 
 export default ChordScreen;
+
+
+
+// TIMER SYNC EXPLANATION : 
+
