f0da2c0e63d76add17f5c4c372108164b04eff8e
Nasser <nassermassadimi@gmail.com>
2022-05-09 19:27:28 +0200

    We implement the function to display the solution The function right_fret() takes 2 inputs : - the note to play (A, B, C...G) - The number of the chord selected randomly by the app (6,5...1),
    This function returns a number ranging from 1 to 12.
    This number is the fret the should play to produce the note randomly
    selected by the app, this is the solution.
    The function has been coded but still need to be displayed on the UI
    timely (using a timer probably or an useEffect -  tbd).

--- fe_warmup_guitar/src/ChordScreen.jsx
+++ fe_warmup_guitar/src/ChordScreen.jsx
@@ -3,8 +3,22 @@ import * as React from "react" ;
 
 const ChordScreen = () => {
     
+// MAIN VARIABLES
+
+    const all_notes = ["A", "B", "C", "D", "E","F", "G"];
+    const all_chords = {
+        6 : "E",
+        5 : "A",
+        4 : "D",
+        3 : "G", 
+        2 : "B",
+        1 : "E", 
+    };   
+
+
+// STATES
     const [note, set_note] = React.useState("A");
-    const [chord, set_chord] = React.useState(6);
+    const [chord, set_chord] = React.useState(4);
     const [delay, set_delay] = React.useState({
         time_typed : "",            // Updated everytime while the user type a new value for the delay
         time_confirmed : 5000,      // Upadted when the user confirm the new delay
@@ -16,22 +30,67 @@ const ChordScreen = () => {
     // We will use it to identify the timer we need to stop with the stop() method of the class Timer
 
 
-
+//CORE FUNCTIONS
     // Change the chords in a random way.
     const mysterious_note = () => {
         console.log("I have been called");
-        const all_notes = ["A","Ab", "A#", "B", "Bb", "C", "C#", "D", "Db", "D#", "E", "Eb","F", "F#", "G", "Gb", "G#"];
-        const all_chords = [6, 5, 4, 3, 2, 1];
         // Choose a random note
-        const random_note = all_notes[Math.round(Math.random() * all_notes.length)];
+        const random_index_note = Math.floor(Math.random() * all_notes.length);
+/*         console.log(`RANDOM NOTE CHORD : ${random_index_note}`) */
+        const random_note = all_notes[random_index_note];
         set_note(random_note);
         // Choose a random chord
-        const random_chord = all_chords[Math.round(Math.random() * all_chords.length)];
-        set_chord(random_chord);
-        console.log(random_note + random_chord);
+        const chords_name = Object.keys(all_chords);
+        const random_index_chord = Math.floor(Math.random() * chords_name.length);
+/*         console.log(`RANDOM INDEX CHORD : ${random_index_chord}`) */
+        const random_chord_number = chords_name[random_index_chord];
+        set_chord(random_chord_number);
+        console.log(random_note + random_chord_number);
         // Create a new Timer (will be started in the useEffect (see below))
         const next_timer = new Timer(delay.time_confirmed);
         set_current_timer(next_timer);
+    };
+
+// Display the solution => The fret where the user should put his/her finger to produce the right note.
+    // input : 1 - the number of the chord (1, 2, 3...etc). | 2 - The note to play (A, B, C#...etc),
+    // output :  the fret to play on the given chord (a number.)
+    // What we need to get to this output ?
+        // The starting point / open_chord_note : the name of the note on fret 0 (E,A,D,G,B or E), 
+        // we just need to calculate the "distance" between the open_chord_note and the note_to_play to find the fret_to_play.
+        // EXAMPLE: E5 ---> 
+
+    const right_fret = (chord_number, note_to_play) => {
+        let fret_to_play;
+        //a - note_to_play = E   || open_chord_note = 5 ==> We deduce that open_chord_note = A.
+        const open_chord_note = all_chords[chord_number];  // output expected : an open chord letter (E, A, D, G, B, E)
+        // b - Between A and E there is ALWAYS a distance of 7 half-step/ frets (this the meat of the algorithm).
+        const chords_grid = {
+            A : 0,
+            A_sharp : 1,
+            Bb : 1,
+            B : 2,
+            C : 3,
+            C_sharp : 4,
+            Db : 4,
+            D : 5, 
+            D_sharp : 6,
+            Eb : 6, 
+            E : 7,
+            F : 8,
+            F_sharp : 9,
+            Gb : 9,
+            G : 10,
+            G_sharp : 11,
+            Ab : 11,
+        }
+        // c - the value to return (fret_to_play) is 7!
+        const number_of_intervals = 12;                 // Why 12 ? Because 12 is the total number of interval (remember C_sharp = Db are the same note!)
+        if (chords_grid[note_to_play] > chords_grid[open_chord_note]) {
+            fret_to_play = chords_grid[note_to_play] - chords_grid[open_chord_note];
+        }else {
+            fret_to_play = 12 + chords_grid[open_chord_note] -chords_grid[note_to_play]; 
+        }
+        return fret_to_play;
     };
 
     // THE TIMER CONSTRUCTOR - 
@@ -96,7 +155,7 @@ const ChordScreen = () => {
     const update_timer = (event) => {
         set_delay({
             ...delay,
-            time_typed : event.target.value
+            time_typed : event.target.value,
         });
     };
 
@@ -105,7 +164,7 @@ const ChordScreen = () => {
         event.preventDefault();
         set_delay({
             time_typed : 0,           
-            time_confirmed : delay.time_typed,
+            time_confirmed : delay.time_typed * 1000,
             new_timer : true, 
         });
     }
